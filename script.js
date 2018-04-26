d1b1be03f4c2cc1245ceb75475ff50024686a568
Talfaran <tal1faran@gmail.com>
2018-04-26 15:57:42 +0300

    recipe challenge - 59.52 second cpability

--- script.js
+++ script.js
@@ -16,70 +16,88 @@ var RecipeApp = function () {
 
     //id's for recipes
     var recId = 0;
-
     //id's for ingredients
     var ingId = 0;
 
-    var createRecipe = function(name, image){
+
+    var findRcpById = function (addIngrButton) {
+
+        for (var i = 0; i < recipes.length; i++) {
+            if (recipes[i].id === currentRecipeId)
+                var currentRecipe = recipes[i];
+            return currentRecipe;
+        }
+    }
+
+
+
+    var createRecipe = function (name, image) {
         var recipe = {
             name: name,
-            image: image, 
+            image: image,
             ingredients: [],
             id: recId
         };
 
         //keeps recipe ids unique 
-        recId ++; 
+        recId++;
 
         recipes.push(recipe);
     };
 
-    var createIngredients = function(){
-        //add code
-    };
-
-    var _getIngredients = function(recipe){
-        var recipesHTML = "";
-
-        //add code
-        return recipesHTML;
-    };
-
-    var renderRecipes = function () {
-        //empty recipes div
-        $recipes.empty();
-
-        for(var i = 0; i < recipes.length; i ++){
-            //current recipe in iteration
-            var recipe = recipes[i];
-
-            //return HTML for all ingredients
-            var ingredients = _getIngredients(); //add code
-
-            $recipes.append(
-                '<div class="recipe col-md-6  offset-md-3 img-fluid shadow" data-id="' + recipe.id + '">' + 
-                    '<h4 class="text-capitalize font-italic text-center">' + recipe.name + '</h4>' +
-                    '<img class="recipe-img" src="' + recipe.image + '"/>' +
-                    '<hr>' +
-                    '<h5 class="font-italic font-bold text-center">ingredients</h5>' +
-                    '<div class="input-group mb-3">' +
-                        '<div class="input-group-prepend">' +
-                            '<span class="add-ingredients input-group-text" id="basic-addon3">Add Ingredients</span>' +
-                        '</div>' + 
-                        '<input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">' +
-                        
-                    '</div>' +
-                    '<ul class="ingredients">' + ingredients + '</ul>'+
-                '</div>'
-            );
+    var createIngredients = function (currentRecipe) {
+        var $currentRecipeId = $(currentRecipe).closest('.recipe').data().id;
+        var recipe = findRcpById(currentRecipeId);
+        var ingredientHTML = {
+            name: $(recipe).siblings('.form-control').val()
         }
+        recipe.ingredients.push(ingridient)
     };
 
-    return {
-        createRecipe: createRecipe,
-        renderRecipes: renderRecipes,
-        // createIngredients: createIngredients,
+    var _getIngredients = function (recipe) {
+        for (var i = 0; i < recipe.ingredients.length; i++) {
+            var indredientsList = indredientsList + reicpe.ingredients[i].name
+        }
+    return (indredientsList);
+};
+
+var renderRecipes = function () {
+    //empty recipes div
+    $recipes.empty();
+
+    for (var i = 0; i < recipes.length; i++) {
+        //current recipe in iteration
+        var recipe = recipes[i];
+
+        //return HTML for all ingredients
+        var ingredients = _getIngredients(recipe); //add code
+
+        $recipes.append(
+            '<div class="recipe col-md-6  offset-md-3 img-fluid shadow" data-id="' + recipe.id + '">' +
+            '<h4 class="text-capitalize font-italic text-center">' + recipe.name + '</h4>' +
+            '<img class="recipe-img" src="' + recipe.image + '"/>' +
+            '<hr>' +
+            '<h5 class="font-italic font-bold text-center">ingredients</h5>' +
+            '<div class="input-group mb-3">' +
+            '<div class="input-group-prepend">' +
+            '<span class="add-ingredients input-group-text" id="basic-addon3">Add Ingredients</span>' +
+            '</div>' +
+            '<input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">' +
+
+            '</div>' +
+            '<ul class="ingredients">' + ingredients + '</ul>' +
+            '</div>'
+        );
     }
+};
+
+return {
+    createRecipe: createRecipe,
+    renderRecipes: renderRecipes,
+    createIngredients: createIngredients,
+    findRcpByClickedBtn: findRcpByClickedBtn,
+    findRcpById: findRcpById
+}
 };
 
 var app = RecipeApp();
@@ -88,7 +106,7 @@ var app = RecipeApp();
 //--------EVENTS
 
 //add a recipe
-$('.add-recipe').on('click', function(){
+$('.add-recipe').on('click', function () {
     //collect input text
     var name = $('#recipe-name').val();
     var image = $('#recipe-image').val();
@@ -98,3 +116,7 @@ $('.add-recipe').on('click', function(){
     app.renderRecipes();
 });
 
+$('.add-recipe').on('click', '.add-ingredients', function () {
+    findRcpByClickedBtn(this)
+    app.createIngredients(_getIngredients(this));
+});
